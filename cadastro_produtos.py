0bfa3a1f74ad8b72b53447836e349360b1851245
Brenna Lyra <blp@cesar.school>
2026-06-01 15:05:37 -0300

    Update cadastro_produtos.py

--- cadastro_produtos.py
+++ cadastro_produtos.py
@@ -1,17 +1,11 @@
 # ==================================================
 #   DADOS || LISTA/DICT DOS PRODUTOS
 # ==================================================
-produtos = {
-  "produto1" : 12,99,
-  "produto2" : 19,99
-}
+    descrição_produtos = [] #lista de dicionários que irão conter a descrição dos produtos
+
 
 # pensei nesse dicionário = {"nome" = ----, "preço" = -----, "categoria/material": ----, "estoque" = -----,
 # e pensei em ser uma lista de dicionários, um pra cada produto (pim: é uma das possíbilidades, só coloquei esse dict como sugestão)
-
-lista_produtos = [] #aqui teria uma lista com os nomes dos produtos na mesma ordem
-
-#pode pensar em outras estruturas de dados
 
 # ==================================================
 #   FUNÇÕES DO CRUD
@@ -21,6 +15,38 @@ lista_produtos = [] #aqui teria uma lista com os nomes dos produtos na mesma ord
 # CREATE  |  cadastro de produto(s) 
 
 #pensei em fazer um mini menu perguntando se quer criar ou nao, e provavelmente um while com uma condicional dentro pra criar um loop de cadastro de produto que pode ser interrompida
+
+def adicionar_produtos():
+
+    add_produto = input("\nDeseja adicionar produtos na loja? [S/N] ").strip().upper()
+
+    while add_produto == "S":
+
+        produto = input("\nDigite o nome do produto: ").strip().capitalize()
+
+        prod_existentes = [] #em duvida se deixo a lista com os nomes dos produtos apenas aqui ou em escopo global, a IA disse que eu tava duplicando dados...
+
+        for p in descrição_produtos: #esse for adiciona apenas a chave nome na lista para ter onde fazer a busca do produto
+            prod_existentes.append (p["nome"])
+                    
+        if produto not in prod_existentes: #verifica se o produto já existe com a lista criada em escopo local com os nomes dos produtos
+            print(f"\nProduto '{produto}' adicionado!")
+
+            print(f"\nAdicione a descrição do produto: ") #criação do dicionário com a descrição do produto
+
+            produtos = {
+                "nome":     produto,
+                "preço":    float(input("Valor do produto: R$ ")),
+                "material": input("Material do produto: ").strip().capitalize(),
+                "estoque":  int(input("Estoque do produto: "))
+            }
+            descrição_produtos.append(produtos) #adiciona o dicionário em uma lista para ter uma sequência de cadastros
+        else:
+            print("\nProduto já existe na loja.")
+
+        add_produto = input("\nDeseja adicionar mais produtos? [S/N] ").strip().upper()
+
+    return descrição_produtos
 
 # READ    |  listar produto(s)
 
@@ -42,3 +68,8 @@ lista_produtos = [] #aqui teria uma lista com os nomes dos produtos na mesma ord
 # ==================================================
 #   Main || LOOP INICIAL
 # ==================================================
+
+print ("====== Adicione produtos na loja ======")
+descrição_produtos = adicionar_produtos()
+
+
