d5789036ab48e0993f50299397b2d7a30238bb43
Brenna Lyra <blp@cesar.school>
2026-06-05 14:49:11 -0300

    Update product list with new items and descriptions

--- dados.py
+++ dados.py
@@ -1,9 +1,19 @@
 # ==================================================
 #   DADOS || LISTA/DICT DOS PRODUTOS
 # ==================================================
-descricao_produtos = [{
-                "nome":     "Item",
+descrição_produtos = [{
+                "nome":     "Escultura",
                 "preço":    12.00,
                 "material": "Madeira",
                 "estoque":  2
-            }]
+            }, {
+                "nome": "Prato Decorado",
+                "preço": 35.00,
+                "material": "Cerâmica",
+                "estoque": 4
+            },{
+                "nome": "Bolsa",
+                "preço": 54.00,
+                "material": "Crochê",
+                "estoque": 7
+            }] 
