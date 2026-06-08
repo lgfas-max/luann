b241a7a58411985f085cc969bdce1fa18d4236b3
Brenna Lyra <blp@cesar.school>
2026-06-08 15:48:21 -0300

    Add menu function for system interface

---
+++ arteso/menu_feiras.py
@@ -0,0 +1,11 @@
+# ==================================================
+#   MENU || "INTERFACE" DO SISTEMA
+# ==================================================
+def menu():
+    print("\n======= MENU =======")
+    print("1 - Inscrição nas Feiras")
+    print("2 - Ler Inscrição")
+    print("3 - Atualizar Inscrição")
+    print("4 - Deletar Inscrição")
+    print("5 - Sair")
+    print("====================")
